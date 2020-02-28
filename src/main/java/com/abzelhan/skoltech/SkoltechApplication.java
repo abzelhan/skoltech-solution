3020cd9fd56f30d727abe6f4936e93bc734b0352
Abzelhan Sahitov <abzelhan.sahitov@robusta.team>
2020-02-28 23:28:37 +0600

    Project skeleton

---
+++ src/main/java/com/abzelhan/skoltech/SkoltechApplication.java
@@ -0,0 +1,13 @@
+package com.abzelhan.skoltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkoltechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkoltechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abzelhan/skoltech/SkoltechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abzelhan.skoltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkoltechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
